e0458246a92028ef435cd4badc4c0423a4554fb1
Ginji Oyama <invincible.gnj1875@gmail.com>
2020-12-25 00:58:43 +0900

    add resetPassword

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { Home, SignIn, SignUp } from './templates';
+import { Home, SignIn, SignUp, Reset } from './templates';
 import Auth from './Auth';
 
 const Router = () => {
@@ -8,6 +8,8 @@ const Router = () => {
     <Switch>
       <Route exact path={"/signup"} component={SignUp} />
       <Route exact path={"/signin"} component={SignIn} />
+      <Route exact path={"/signin/reset"} component={Reset} />
+
       <Auth>
         <Route exact path={"(/)?"} component={Home} />
       </Auth>
