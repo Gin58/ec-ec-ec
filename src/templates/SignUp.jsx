b62aa9b61e9541cb55c8f243048b61909e2bf63b
Ginji Oyama <invincible.gnj1875@gmail.com>
2020-12-24 22:10:46 +0900

    add signUp operations

--- src/templates/SignUp.jsx
+++ src/templates/SignUp.jsx
@@ -1,7 +1,10 @@
 import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { PrimaryButton, TextInput } from '../components/UIkit';
+import { signUp } from '../reducks/users/operations';
 
 const SignUp = () => {
+  const dispatch = useDispatch();
 
   const [username, setUsername] = useState(""),
         [email, setEmail] = useState(""),
@@ -73,7 +76,7 @@ const SignUp = () => {
       <div className="center">
         <PrimaryButton
           label={"アカウントを登録する"}
-          onClick={() => console.log('Clicked!')}
+          onClick={() => dispatch(signUp(username, email, password, confirmPassword))}
         />
       </div>
     </div>
