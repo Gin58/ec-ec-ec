a92947d7f4b1b824c494a1e3cebc19a6a76265c0
Ginji Oyama <invincible.gnj1875@gmail.com>
2020-12-28 19:42:18 +0900

    add edit product

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,11 +1,17 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { TextInput, SelectBox, PrimaryButton } from '../components/UIkit';
 import { useDispatch } from 'react-redux';
 import { saveProduct } from '../reducks/products/operations';
 import ImageArea from '../components/Products/ImageArea';
+import { db } from '../firebase/index';
 
 const ProductEdit = () => {
   const dispatch = useDispatch();
+  let id = window.location.pathname.split('/product/edit')[1];
+
+  if (id !== '') {
+    id = id.split('/')[1];
+  }
 
   const [name, setName] = useState(''),
         [description, setDescription] = useState(''),
@@ -36,7 +42,22 @@ const ProductEdit = () => {
     {id: 'all', name: '全て'},
     {id: 'male', name: 'メンズ'},
     {id: 'female', name: 'レディース'}
-  ]
+  ];
+
+  useEffect(() => {
+    if (id !== '') {
+      db.collection('products').doc(id).get()
+        .then(snapshot => {
+          const data = snapshot.data();
+          setImages(data.images);
+          setName(data.name);
+          setDescription(data.description);
+          setPrice(data.price);
+          setGender(data.gender);
+          setCategory(data.category);
+        });
+    }
+  }, [id])
 
   return (
     <section>
@@ -94,7 +115,7 @@ const ProductEdit = () => {
         <div className='center'>
           <PrimaryButton
            label={'商品を保存'}
-           onClick={() => dispatch(saveProduct(name, description, category, gender, price, images))}
+           onClick={() => dispatch(saveProduct(id, name, description, category, gender, price, images))}
           />
         </div>
       </div>
