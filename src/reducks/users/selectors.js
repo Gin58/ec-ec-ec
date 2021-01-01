58f7a8e872cbeb6688c1d1b6f927b0cc26e40087
Ginji Oyama <invincible.gnj1875@gmail.com>
2021-01-02 01:27:10 +0900

    add addProductToCart

--- src/reducks/users/selectors.js
+++ src/reducks/users/selectors.js
@@ -1,18 +1,20 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
 
 const usersSelector = (state) => state.users;
 
+export const getProductsInCart = createSelector(
+  [usersSelector],
+  (state) => state.cart
+);
+
 export const getIsSignedIn = createSelector(
   [usersSelector],
-  state => state.isSignedIn
-)
-
-export const getUserId = createSelector(
-  [usersSelector],
-  state => state.uid
+  (state) => state.isSignedIn
 );
+
+export const getUserId = createSelector([usersSelector], (state) => state.uid);
 
 export const getUsername = createSelector(
   [usersSelector],
-  state => state.username
+  (state) => state.username
 );
