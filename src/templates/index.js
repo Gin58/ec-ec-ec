213f528b0c107b5b4d5cd94e2504b86a92ea594d
Ginji Oyama <invincible.gnj1875@gmail.com>
2021-01-03 00:18:09 +0900

    add cart-list-item

--- src/templates/index.js
+++ src/templates/index.js
@@ -5,3 +5,4 @@ export { default as SignUp } from "./SignUp";
 export { default as ProductEdit } from "./ProductEdit";
 export { default as ProductList } from "./ProductList";
 export { default as ProductDetail } from "./ProductDetail";
+export { default as CartList } from "./CartList";
