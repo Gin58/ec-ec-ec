c20d86be7f73686ace202a953b4e1d565dec1938
Ginji Oyama <invincible.gnj1875@gmail.com>
2021-01-03 16:18:29 +0900

    fix

--- src/reducks/products/operations.js
+++ src/reducks/products/operations.js
@@ -41,7 +41,7 @@ export const orderProduct = (productsInCart, amount) => {
     const userRef = db.collection("users").doc(uid);
     const timestamp = FirebaseTimestamp.now();
 
-    let products = {},
+    let products = [],
       soldOutProducts = [];
 
     const batch = db.batch();
@@ -65,7 +65,7 @@ export const orderProduct = (productsInCart, amount) => {
         }
       });
 
-      products[product.productId] = {
+      products.push = {
         id: product.productId,
         images: product.images,
         name: product.name,
