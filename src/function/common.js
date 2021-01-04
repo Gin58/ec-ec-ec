33434e49f8da106e27ebfd2aa311259568b6d8a9
Ginji Oyama <invincible.gnj1875@gmail.com>
2021-01-04 22:48:38 +0900

    add validator

--- src/function/common.js
+++ src/function/common.js
@@ -7,3 +7,13 @@ export const returnCodeToBr = (text) => {
     return HTMLReactParser(text.replace(/\r?\n/g, "<br/>"));
   }
 };
+
+export const isValidRequiredInput = (...args) => {
+  let validator = true;
+  for (let i = 0; i < args.length; i = (i + 1) | 0) {
+    if (args[i] === "") {
+      validator = false;
+    }
+  }
+  return validator;
+};
